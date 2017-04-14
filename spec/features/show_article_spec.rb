3a3205132142f3f6f74fcba87f5de61e38c37954
Hodari Toure <hodari@hiddengeniusproject.org>
2017-04-14 12:17:11 -0700

    implement Showing Article Details

---
+++ spec/features/show_article_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "Showing an article" do
+
+  before do
+    @article = Article.create(title: "The first article", body: "Lorem ipsum dloar sit amet, consecctoetur.")
+  end
+
+  scenario "A user lists all articles" do
+    visit "/"
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+  end
+
+end
